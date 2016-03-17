d8f807dbec611789da9ed0e07c6721c44b161e67
Peter2 <peterpig2@yahoo.com.tw>
2016-03-17 16:24:07 +0800

    新增Servlet、web.xml、index.jsp

---
+++ ProjWeb02/src/view/Servlet.java
@@ -0,0 +1,33 @@
+package view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out=response.getWriter();
+		out.println("<html><head>");
+		out.println("<title>Demo</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h3>Hello World!</h3>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
